203715b0ba979d96556aaae42e5690981a3ac25d
YBsaw <ah_boon95@hotmail.com>
2015-04-30 02:40:50 +0800

    New version of lassie

---
+++ Lassies/src/GameStates/GameStateManager.java
@@ -0,0 +1,114 @@
+package GameStates;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JOptionPane;
+
+import Audio.Audio;
+import Entity.HUD;
+import Main.GameLoop;
+
+public class GameStateManager {
+	
+	public static GameState[] gameStates;
+	public static int currentState;
+	
+	public static final int numStates = 7;
+	public static final int MENU = 0;
+	public static final int LEVEL = 1;
+	public static final int LEVEL1 = 2;
+	public static final int LEVEL2 = 3;
+	public static final int LEVEL3 = 4;
+	public static final int LEVEL4 = 5;
+	public static final int LEVEL5 = 6;
+	
+	public GameStateManager() {
+		
+		gameStates = new GameState[numStates];
+		
+		currentState = MENU;
+		loadState(currentState);	
+	}
+	
+	public static void unloadState(int state){
+		gameStates[state] = null;
+	}
+	
+	public static void loadState(int state){
+		if (state == MENU) gameStates[state] = new Menu();
+		if (state == LEVEL) gameStates[state] = new Level();
+		if (state == LEVEL1) gameStates[state] = new Level1();
+		if (state == LEVEL2) gameStates[state] = new Level2();
+		if (state == LEVEL3) gameStates[state] = new Level3();
+		if (state == LEVEL4) gameStates[state] = new Level4();
+		if (state == LEVEL5) gameStates[state] = new Level5();
+	}
+	
+	public static void setState(int state) {
+		unloadState(currentState);
+		currentState = state;
+		loadState(currentState);
+	}
+	
+	public void update() {
+		try{
+			gameStates[currentState].update();
+		}catch(Exception e){}
+	}
+	
+	public void draw(Graphics2D g) {
+		try{
+			gameStates[currentState].draw(g);
+		}catch(Exception e){} 
+	}
+	
+	public static int getState(){ return currentState;}
+	
+	@SuppressWarnings({ "deprecation"})
+	public void keyPressed(int p) {
+		gameStates[currentState].keyPressed(p);
+		if (currentState > 1 && p == KeyEvent.VK_P){
+			try {
+				Audio.music[GameStateManager.currentState-1].stop();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			HUD.timer.stop();
+			GameLoop.thread.suspend();
+			Object[] options = {"Resume","Restart","Main Menu", "Quit"};
+			int n = JOptionPane.showOptionDialog(null,
+					"Paused",
+					"Lassie",
+					JOptionPane.YES_NO_CANCEL_OPTION,
+					JOptionPane.INFORMATION_MESSAGE,
+					null,     
+					options, 
+					options[0]);
+	
+		switch (n){
+		case 0:
+			HUD.timer.start();
+			Audio.music[GameStateManager.currentState-1].resume();
+			GameLoop.thread.resume();
+			break;
+		case 1:
+			setState(currentState);
+			GameLoop.thread.resume();
+			break;
+		case 2:
+			setState(MENU);
+			GameLoop.thread.resume();
+			break;
+		case 3:
+			System.exit(0);
+			break;
+		}
+	}
+}
+
+	
+	public void keyReleased(int r) {
+		gameStates[currentState].keyReleased(r);
+	}
+}
---
+++ Lassies/src/GameStates/Level.java
@@ -0,0 +1,108 @@
+package GameStates;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+
+import Audio.Audio;
+import File.Load;
+import Map.Background;
+
+public class Level extends GameState{
+
+private Background bg;
+	
+	private Font titleFont;
+	private Font menuFont;
+	private Color titleColor;
+	
+	public static int lastState;	
+	private int currentChoice = 0;
+	private String[] menu = {"1","2","3","4","5"};
+	
+	public Level()
+	{
+		init();
+	}
+
+	
+	public void init() {
+		try{
+			bg = new Background("/Backgrounds/menubg.gif", 1);
+			bg.animation(-0.1, 0);
+			
+			titleColor = new Color(128, 0, 0);
+			titleFont = new Font("Century Gothic", Font.PLAIN, 28);
+			
+			menuFont = new Font("Arial", Font.PLAIN, 12);
+			
+			lastState = Load.load();
+			
+			if (lastState < 1)	lastState = 2;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+
+	public void update() 
+	{
+		bg.update();
+	}
+
+	public void draw(Graphics2D g) 
+	{
+		bg.draw(g);
+		
+		g.setFont(titleFont);
+		g.setColor(titleColor);
+		g.drawString("Select Level", 30, 70);
+		
+		g.setFont(menuFont);
+		for (int counter=0; counter < menu.length; counter++){
+			if (counter == currentChoice) g.setColor(Color.GREEN);
+			else if (Integer.parseInt(menu[counter]) > lastState-1) g.setColor(Color.GRAY);
+			else g.setColor(Color.BLACK);
+			g.drawString(menu[counter], 145, 140 + counter * 15);
+		}
+	}
+	
+	public void select()
+	{
+		Audio.music[GameStateManager.currentState-1].stop();
+		switch (this.currentChoice){
+		case 0: 
+			GameStateManager.setState(GameStateManager.LEVEL1);
+			break;
+		case 1: 
+			GameStateManager.setState(GameStateManager.LEVEL2);
+			break;
+		case 2:
+			GameStateManager.setState(GameStateManager.LEVEL3);
+			break;
+		case 3:
+			GameStateManager.setState(GameStateManager.LEVEL4);
+			break;
+		case 4:
+			GameStateManager.setState(GameStateManager.LEVEL5);
+			break;
+		}
+	}
+	
+	public void keyPressed(int p)
+	{
+		if (p == KeyEvent.VK_ENTER)	select();
+		if (p == KeyEvent.VK_UP){
+			currentChoice--;
+			if (currentChoice <0)	currentChoice = lastState-2;
+		}
+		if (p == KeyEvent.VK_DOWN){
+			currentChoice++;
+			if (currentChoice == lastState - 1)	currentChoice = 0;
+		}	
+	}
+	
+	public void keyReleased(int r){}
+}
